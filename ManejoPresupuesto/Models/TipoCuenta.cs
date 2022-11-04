17459c821bc373f1917f290f0abd4beff827745e
Angelo Gonzalez <angelo.gonzalez@live.com>
2022-11-03 22:16:32 -0300

    109. Comunicándonos con la Base de Datos - Connection Strings

--- ManejoPresupuesto/Models/TipoCuenta.cs
+++ ManejoPresupuesto/Models/TipoCuenta.cs
@@ -3,27 +3,13 @@ using System.ComponentModel.DataAnnotations;
 
 namespace ManejoPresupuesto.Models
 {
-    public class TipoCuenta: IValidatableObject
+    public class TipoCuenta
     {
         public int Id { get; set; }
-
         [Required(ErrorMessage = "El campo {0} es requerido")]
-        //[PrimeraLetraMayuscula]
+        [PrimeraLetraMayuscula]
         public string Nombre { get; set; }
         public int UsuarioId { get; set; }
-        public int Orden { get; set; }
-
-        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
-        {
-            if (Nombre != null && Nombre.Length > 0)
-            {
-                var primeraLetra = Nombre[0].ToString();
-
-                if (primeraLetra != primeraLetra.ToUpper())
-                {
-                    yield return new ValidationResult("La primera letra debe ser mayúscula", new[] { nameof(Nombre) });
-                }
-            }
-        }
+        public int Orden { get; set; }       
     }
 }
