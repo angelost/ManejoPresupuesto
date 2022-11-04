9c11fc19c7ddea547747ec05a21b8f06f35ac896
Angelo Gonzalez <angelo.gonzalez@live.com>
2022-11-04 00:19:18 -0300

    115. Listado Tipos Cuentas

--- ManejoPresupuesto/Servicios/RepositorioTiposCuentas.cs
+++ ManejoPresupuesto/Servicios/RepositorioTiposCuentas.cs
@@ -8,6 +8,7 @@ namespace ManejoPresupuesto.Servicios
     {
         Task Crear(TipoCuenta tipoCuenta);
         Task<bool> Existe(string nombre, int usuarioId);
+        Task<IEnumerable<TipoCuenta>> Obtener(int usuarioId);
     }
     public class RepositorioTiposCuentas : IRepositorioTiposCuentas
     {
@@ -37,6 +38,15 @@ namespace ManejoPresupuesto.Servicios
                                                                         WHERE Nombre = @Nombre AND UsuarioId = @UsuarioId;",
                                                                         new { nombre, usuarioId });
             return existe == 1;
+        }
+
+        public async Task<IEnumerable<TipoCuenta>> Obtener(int usuarioId)
+        {
+            using var connection = new SqlConnection(connectionString);
+            return await connection.QueryAsync<TipoCuenta>(@"SELECT Id, Nombre, Orden
+                                                            FROM TipoCuentas
+                                                            WHERE UsuarioId = @UsuarioId",
+                                                            new { usuarioId });
         }
 
     }
