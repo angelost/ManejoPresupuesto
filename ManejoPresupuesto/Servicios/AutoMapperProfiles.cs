750c9dedd5212e0ef07f3d29b6032fd4afaf8579
Angelo Gonzalez <angelo.gonzalez@live.com>
2022-11-05 15:35:25 -0300

    131. Utilizando AutoMapper

---
+++ ManejoPresupuesto/Servicios/AutoMapperProfiles.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using ManejoPresupuesto.Models;
+using System.Transactions;
+
+namespace ManejoPresupuesto.Servicios
+{
+    public class AutoMapperProfiles : Profile
+    {
+        public AutoMapperProfiles()
+        {
+            // Desde -> Hacia
+            CreateMap<Cuenta, CuentaCreacionViewModel>();            
+        }
+    }
+}
